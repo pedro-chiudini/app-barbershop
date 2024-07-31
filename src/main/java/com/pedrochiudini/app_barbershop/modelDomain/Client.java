6fac207e31101f4cf4da7d30ffdfe31a7111ae7a
pedro chiudini <pedrochiudinil@gmail.com>
2024-07-31 11:33:40 -0300

    Finalizando a classe cliente

--- src/main/java/com/pedrochiudini/app_barbershop/modelDomain/Client.java
+++ src/main/java/com/pedrochiudini/app_barbershop/modelDomain/Client.java
@@ -7,8 +7,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.pedrochiudini.app_barbershop.util.UserRole;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +25,9 @@ public class Client extends User implements UserDetails{
 
     private String password;
     @Column(nullable = false)
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -36,6 +43,26 @@ public class Client extends User implements UserDetails{
     @Override
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
     
 }
