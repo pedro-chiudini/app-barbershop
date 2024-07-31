61c2e053902a15e174924f9cbdd1089b2df1c640
pedro chiudini <pedrochiudinil@gmail.com>
2024-07-31 18:34:19 -0300

    Finalizando a migration de agendamentos

---
+++ src/main/resources/db/migration/V4__create-table-schedules.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tb_schedules (
+    id BIGINT NOT NULL PRIMARY KEY,
+    date DATE NOT NULL,
+    time VARCHAR(50) NOT NULL,
+    price FLOAT NOT NULL,
+    status ENUM('CONFIRMADO', 'CONCLUIDO', 'CANCELADO') NOT NULL,
+    barber_id BIGINT NOT NULL,
+    client_id BIGINT NOT NULL,
+    service_id BIGINT NOT NULL,
+    FOREIGN KEY (barber_id) REFERENCES tb_barbers(id),
+    FOREIGN KEY (client_id) REFERENCES tb_clients(id),
+    FOREIGN KEY (service_id) REFERENCES tb_services(id)
+);
