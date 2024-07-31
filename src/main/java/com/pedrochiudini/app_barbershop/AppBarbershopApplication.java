690a5e20b195bb6130db962587bd926b9e63b6bf
pedro chiudini <pedrochiudinil@gmail.com>
2024-07-31 10:09:39 -0300

    Primeiro commit

---
+++ src/main/java/com/pedrochiudini/app_barbershop/AppBarbershopApplication.java
@@ -0,0 +1,13 @@
+package com.pedrochiudini.app_barbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBarbershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBarbershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrochiudini/app_barbershop/AppBarbershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrochiudini.app_barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBarbershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
