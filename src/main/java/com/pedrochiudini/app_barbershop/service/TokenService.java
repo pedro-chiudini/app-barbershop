43e48f3dd4db8706fbebcdc895a124ac5064c480
pedro chiudini <pedrochiudinil@gmail.com>
2024-08-01 19:40:45 -0300

    Criando o service de token authorizations

---
+++ src/main/java/com/pedrochiudini/app_barbershop/service/TokenService.java
@@ -0,0 +1,49 @@
+package com.pedrochiudini.app_barbershop.service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.pedrochiudini.app_barbershop.modelDomain.Client;
+
+@Service
+public class TokenService {
+    
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Client client) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(getExpirantionDate())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error while generation token", e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTCreationException e) {
+            return "";
+        }
+    }
+
+    private Instant getExpirantionDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
