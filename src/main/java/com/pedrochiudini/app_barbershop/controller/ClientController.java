d98263f54458717a683da2d353d8c64e7a921ec6
pedro chiudini <pedrochiudinil@gmail.com>
2024-08-01 20:58:09 -0300

    Finalizando a camada controller do cliente

---
+++ src/main/java/com/pedrochiudini/app_barbershop/controller/ClientController.java
@@ -0,0 +1,43 @@
+package com.pedrochiudini.app_barbershop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrochiudini.app_barbershop.dto.LoginDTO;
+import com.pedrochiudini.app_barbershop.dto.LoginResponseDTO;
+import com.pedrochiudini.app_barbershop.dto.RegisterDTO;
+import com.pedrochiudini.app_barbershop.service.ClientService;
+
+@RestController
+@RequestMapping("/c")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerClient(@RequestBody RegisterDTO data) {
+        try {
+            String response = clientService.register(data);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.toString());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> loginClient(@RequestBody LoginDTO data) {
+        try {
+            LoginResponseDTO responseDTO = clientService.login(data);
+            return ResponseEntity.ok(responseDTO);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+}
