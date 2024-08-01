a25260f8ad31a9eb3f479d96d4c49b44f0b22595
pedro chiudini <pedrochiudinil@gmail.com>
2024-08-01 19:36:24 -0300

    Finalizando o serviço de autorização

--- src/main/java/com/pedrochiudini/app_barbershop/service/AuthorizationService.java
+++ src/main/java/com/pedrochiudini/app_barbershop/service/AuthorizationService.java
@@ -2,20 +2,21 @@ package com.pedrochiudini.app_barbershop.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.pedrochiudini.app_barbershop.repository.ClientRepository;
 
 @Service
-public class AuthorizationService {
+public class AuthorizationService implements UserDetailsService {
     
     @Autowired
     ClientRepository clientRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username);
+        return clientRepository.findByUsername(username);
     }
 
 }
