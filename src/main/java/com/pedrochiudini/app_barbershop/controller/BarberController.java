53fd9541598e99744e055e8b22090f616bee4607
pedro chiudini <pedrochiudinil@gmail.com>
2024-08-05 18:05:12 -0300

    Iniciando as camadas de negocio da classe barber

---
+++ src/main/java/com/pedrochiudini/app_barbershop/controller/BarberController.java
@@ -0,0 +1,5 @@
+package com.pedrochiudini.app_barbershop.controller;
+
+public class BarberController {
+    
+}
---
+++ src/main/java/com/pedrochiudini/app_barbershop/service/BarberService.java
@@ -0,0 +1,8 @@
+package com.pedrochiudini.app_barbershop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BarberService {
+    
+}
